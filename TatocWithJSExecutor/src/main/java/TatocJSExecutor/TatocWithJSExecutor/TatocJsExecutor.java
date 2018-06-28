e6d657e3314bf8747c27a115ee16754fb0236883
RajKeshriQA <40289602+RajKeshriQA@users.noreply.github.com>
2018-06-28 14:12:20 +0530

    Add files via upload

---
+++ TatocWithJSExecutor/src/main/java/TatocJSExecutor/TatocWithJSExecutor/TatocJsExecutor.java
@@ -0,0 +1,107 @@
+package TatocJSExecutor.TatocWithJSExecutor;
+
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TatocJsExecutor{
+	WebDriver driver ;
+	JavascriptExecutor js;
+	public void initializeBrowser() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\rajkeshri\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		js= (JavascriptExecutor)driver;
+	}
+	
+	public String getCurrentLink() {
+		return driver.getCurrentUrl();
+	}
+	
+	public void openBrowserAndChooseBasic() {
+		driver.get("http://10.0.1.86/tatoc");
+		
+		WebElement BasicCourse = driver.findElement(By.linkText("Basic Course"));
+		js.executeScript("arguments[0].click();", BasicCourse);
+		}
+	
+	public void greenBox() {
+
+		WebElement greenBox = driver.findElement(By.className("greenbox"));
+		js.executeScript("arguments[0].click();", greenBox);
+
+		}
+	public void frameDungeon() {
+	driver.switchTo().frame("main");
+	String box1 = driver.findElement(By.id("answer")).getAttribute("class");
+	
+	WebElement proceed=driver.findElement(By.linkText("Proceed"));
+	while(true){
+			WebElement repaintBox=driver.findElement(By.linkText("Repaint Box 2"));
+			js.executeScript("arguments[0].click();", repaintBox);
+			driver.switchTo().frame("child"); 
+			String box2 = driver.findElement(By.id("answer")).getAttribute("class");
+			driver.switchTo().parentFrame(); 
+
+			if(box1.equals(box2))
+			{
+				js.executeScript("arguments[0].click();", proceed);
+				break;
+			}
+	     }
+	driver.switchTo().defaultContent();
+	}
+
+	public void dragAndDrop() {
+		Actions act=new Actions(driver);
+		
+		WebElement dg=driver.findElement(By.id("dragbox"));
+		WebElement dp=driver.findElement(By.id("dropbox"));
+		act.dragAndDrop(dg, dp).build().perform();
+		 
+		 WebElement drag_proceed = driver.findElement(By.linkText("Proceed"));
+		 js.executeScript("arguments[0].click();",drag_proceed);
+	}
+	
+	public void popUpWindow() {
+		
+	 WebElement popup =  driver.findElement(By.linkText("Launch Popup Window"));
+	 js.executeScript("arguments[0].click();",popup);
+	 
+	 ArrayList<String> tabs2 = new ArrayList<String> (driver.getWindowHandles());
+     driver.switchTo().window(tabs2.get(1));
+     
+     WebElement sc = driver.findElement(By.cssSelector("#name"));
+     js.executeScript("document.getElementById('name').value='hello world';");
+     
+     WebElement sub = driver.findElement(By.cssSelector("#submit"));
+     js.executeScript("arguments[0].click();",sub);
+     driver.switchTo().window(tabs2.get(0));
+     
+     WebElement pro = driver.findElement(By.linkText("Proceed"));
+     js.executeScript("arguments[0].click();",pro);
+	}
+	
+	public void cookie() {
+		
+		 WebElement token = driver.findElement(By.linkText("Generate Token"));
+         js.executeScript("arguments[0].click();",token);
+         
+         String Token = driver.findElement(By.id("token")).getText();
+         String[] splited = Token.split("\\s+"); 
+         Cookie token1 = new Cookie("Token", splited[1]);
+         driver.manage().addCookie(token1);
+         WebElement tok = driver.findElement(By.linkText("Proceed"));
+         js.executeScript("arguments[0].click();",tok);
+       
+	}  
+	 
+	
+}
